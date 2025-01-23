5ad6201cb0f2b63d450cc57016896de839f88f03
AbdulHannan003 <terminator.zed2018@gmail.com>
2025-01-23 17:43:53 +0500

    something

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,15 +1,30 @@
-import Switcher from '@/components/ui/Switcher'
-import type { ChangeEvent } from 'react'
+import { AdaptiveCard } from '@/components/shared'
+import { Card, Button } from '@/components/ui'
 
 const Home = () => {
-    const onSwitcherToggle = (val: boolean, e: ChangeEvent) => {
-        console.log(val, e)
-    }
-
     return (
-        <div>
-            <Switcher defaultChecked onChange={onSwitcherToggle} />
-        </div>
+        <main className="flex flex-row gap-4">
+            <div className="w-9/12">
+                <AdaptiveCard>
+                    <div className="flex flex-row w-full justify-between items-center">
+                        <p className="text-2xl font-bold text-black">
+                            Overview
+                        </p>
+                        <Button variant="default">All Claims</Button>
+                    </div>
+                    <div className="grid grid-cols-3 gap-4 w-full bg-[#F2F2F2] p-1 rounded-[20px]">
+                        <Card bordered={false}></Card>
+                        <Card bordered={false}></Card>
+                        <Card bordered={false}></Card>
+                    </div>
+                </AdaptiveCard>
+            </div>
+            <div className="w-3/12">
+                <AdaptiveCard>
+                    <p>Overview</p>
+                </AdaptiveCard>
+            </div>
+        </main>
     )
 }
 
